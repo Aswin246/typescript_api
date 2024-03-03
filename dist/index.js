d0ce0829315f6a6eff469cf159acbf9582299b7e
Aswin246 <mashwinarjun@gmail.com>
2024-03-03 13:31:23 +0530

    typescript api's

---
+++ dist/index.js
@@ -0,0 +1,55 @@
+"use strict";
+// interface User {
+//   name: string;
+//   age: number;
+// }
+// const sumOfAge = (user1: User, user2: User) => {
+//   return user1.age + user2.age;
+// };
+// const result = sumOfAge(
+//   {
+//     name: "Aswin",
+//     age: 22,
+//   },
+//   {
+//     name: "ABC",
+//     age: 54,
+//   }
+// );
+// console.log(result);
+// interface User {
+//   name: string;
+//   age: number;
+//   email: string;
+//   pass: string;
+// }
+// type updateOnly = Pick<User, "name" | "age">;
+// type partialUpdate = Partial<updateOnly>;
+// const display = (updated: partialUpdate) => {
+//   console.log(updated.name + " " + updated.age);
+// };
+// display({
+//   name: "Aswin",
+// });
+// type user1 = {
+//   readonly name: string;
+//   readonly age: number;
+// };
+// const users: user1 = {
+//   age: 21,
+//   name: "Aswin",
+// };
+// // users.name = "ABC";
+// console.log(users.name);
+// type users = {
+//   name: string;
+//   age: number;
+// };
+// type user = Record<string, users>;
+// const user1: user = {
+//   abc: { name: "ABCS", age: 21 },
+// };
+// console.log(user1);
+const user = new Map();
+user.set("id1", { name: "ABC", age: 21 });
+console.log(user.get("id1"));
